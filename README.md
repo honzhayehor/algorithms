edcc0d78664326ed87e8cc4047011a008a430a5d
Honzha Yehor <honzhayehor@gmail.com>
2026-02-09 21:30:44 +0200

    First lines of code of Binary search algorithm implementation.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 This repository is my attempt to implement basic algorithms (sorting, searching, etc).
 
+At first, I plan to divide algorithms into 3 sections*:
+1. Sorting
+2. Searching
+3. Graph
+
+That can provide a good structure to my library, but I can change my mind.
+
+\* - shoutout to ForrestKnight (https://www.youtube.com/@fknight) as he inspired me to use that algorithm division. 
---
+++ src/main/java/search/BinarySearch.java
@@ -0,0 +1,22 @@
+package search;
+
+import java.util.Collection;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Implementation of Binary search algorithm.
+ * <p>Class contains several static methods that work with Java Collection framework
+ */
+public class BinarySearch {
+    public static <T> int search(Collection<? extends T> list, T elementToFind) {
+        Object[] arr = list.toArray();
+
+        int midPoint = arr.length - 1;
+        int start = 0;
+        while (true) {
+            break;
+        }
+
+        return -1;
+    }
+}
