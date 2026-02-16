4311218dc57cbc5c20f55da6f0230961db106b3c
Honzha Yehor <honzhayehor@gmail.com>
2026-02-16 21:19:08 +0200

    Created nested classes Edge and Node in Graph class.

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -1,48 +1,82 @@
 package graph;
 
-import org.jspecify.annotations.Nullable;
-import java.util.ArrayList;
-import java.util.List;
+import org.jspecify.annotations.NonNull;
+
+import java.util.*;
 
 
 public final class Graph {
-    private final List<Node> NODES = new ArrayList<>();
+    private final int graphId;
+    private static int lastGraphID = 0;
+    private final Map<Node, List<Edge>> nodes = new HashMap<>(); // TODO: Change to Map<Node, List<Edge>>, since all nodes are effectively final with ID
+    private int lastNodeID = 0;
+    private int lastEdgeID = 0;
 
-    Graph() {}
+    public Graph() {
+        this.graphId = ++lastGraphID;
+    }
 
-    public class Node {
-        private final int DISTANCE;
-        @Nullable
-        private Node nextNode;
-        @Nullable
-        private Node previousNode;
+    //TODO: create methods for creating, removing and connecting nodes
 
-        Node(int distance, @Nullable Node nextNode, @Nullable Node previousNode) {
-            DISTANCE = distance;
-            this.nextNode = nextNode;
-            this.previousNode = previousNode;
+    static final class Edge {
+        private final int graphId;
+        private int distance;
+        private final int id;
+        private final Node target;
+
+        Edge(int distance, @NonNull Node target, int id, int graphId) {
+            changeDistance(distance);
+            this.target = target;
+            this.id = id;
+            this.graphId = graphId;
         }
 
-        Node(int distance) {
-            DISTANCE = distance;
+        public void changeDistance(int distance) {
+            if (distance <= 0) {
+                throw new IllegalArgumentException("Distance cannot be negative or zero");
+            }
+            this.distance = distance;
+        }
+        public int getDistance() {return distance;}
+
+        public Node getTarget() {
+            return target;
         }
 
-        public void setNextNode(@Nullable Node nextNode) {
-            this.nextNode = nextNode;
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Edge edge)) return false;
+            return id == edge.id && graphId == edge.graphId;
         }
 
-        public void setPreviousNode(@Nullable Node previousNode) {
-            this.previousNode = previousNode;
+        @Override
+        public int hashCode() {
+            return Objects.hash(graphId, id);
+        }
+    }
+
+    static final class Node {
+        private final int id;
+        private final int graphId;
+
+        Node(int id, int graphId) {
+            this.id = id;
+            this.graphId = graphId;
         }
 
-        public boolean hasNextNode() {
-            return nextNode == null;
+        int getId() {return id;}
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Node node)) return false;
+            return id == node.id && graphId == node.graphId;
         }
 
-        public boolean hasPreviousNode() {
-            return previousNode == null;
+        @Override
+        public int hashCode() {
+            return Objects.hash(graphId, id);
         }
-
-
     }
 }
