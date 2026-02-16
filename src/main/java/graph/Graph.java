fa9ae47ccd7b30115290e24ff16076d092259a0f
Honzha Yehor <honzhayehor@gmail.com>
2026-02-16 14:32:48 +0200

    Further development of Graph class

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -1,16 +1,48 @@
 package graph;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import org.jspecify.annotations.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Graph {
-    private final PriorityQueue<Node> queue;
 
-    Graph(Comparator<Node> comparator) {
-        queue = new PriorityQueue<>(comparator);
-    }
+public final class Graph {
+    private final List<Node> NODES = new ArrayList<>();
+
+    Graph() {}
 
     public class Node {
+        private final int DISTANCE;
+        @Nullable
+        private Node nextNode;
+        @Nullable
+        private Node previousNode;
+
+        Node(int distance, @Nullable Node nextNode, @Nullable Node previousNode) {
+            DISTANCE = distance;
+            this.nextNode = nextNode;
+            this.previousNode = previousNode;
+        }
+
+        Node(int distance) {
+            DISTANCE = distance;
+        }
+
+        public void setNextNode(@Nullable Node nextNode) {
+            this.nextNode = nextNode;
+        }
+
+        public void setPreviousNode(@Nullable Node previousNode) {
+            this.previousNode = previousNode;
+        }
+
+        public boolean hasNextNode() {
+            return nextNode == null;
+        }
+
+        public boolean hasPreviousNode() {
+            return previousNode == null;
+        }
+
 
     }
 }
