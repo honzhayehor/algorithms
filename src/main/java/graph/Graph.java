0d88b485a45f35fff364d7e3f12eb01cf093f54d
Honzha Yehor <honzhayehor@gmail.com>
2026-02-17 18:15:41 +0200

    New API methods added to Graph class that enable to create, connect and remove nodes

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -3,6 +3,7 @@ package graph;
 import org.jspecify.annotations.NonNull;
 
 import java.util.*;
+import com.google.common.base.Optional;
 
 
 public final class Graph {
@@ -31,12 +32,46 @@ public final class Graph {
         return newNode;
     }
 
+    /**
+     * Connects two nodes and setting distance between them.
+     * @param from Starting Node that needs to be connected to the next one
+     * @param to Node to which the 'from' node is pointing (connected)
+     * @param distance integer that describes distance between two nodes (weights)
+     * @throws IllegalArgumentException in multiple cases: Node 'from' points to itself, Node ('from' or 'to') is null and if Node does not belong to current graph
+     * */
     public void connectNodes(Node from, Node to, int distance) {
         validateNodes(from, to);
         Edge edge = new Edge(distance, to, ++lastEdgeID, this.graphId);
         nodes.get(from).put(to, edge);
     }
 
+    /**
+     * Removes specified Node from the graph
+     * @param node Node that needs to be removed
+     * @return Optional that may contain removed Node value
+     * @throws IllegalArgumentException if given node is not from this graph
+     * */
+    public boolean removeNode(Node node) {
+        validateNode(node);
+        nodes.remove(node);
+
+        for (Map<Node, Edge> edges : nodes.values()) {
+            edges.remove(node);
+        }
+        return true;
+    }
+
+    public boolean disconnectNodes(Node from, Node to) {
+        validateNode(from);
+        validateNode(to);
+        return nodes.get(from).remove(to) != null;
+    }
+
+    /**
+     * Validates node object for null-safety and affiliation to current graph
+     * @param node Node that need to be validated
+     * @throws IllegalArgumentException if node is null or does not belong to current graph
+     * */
     private void validateNode(Node node) {
         if (node == null) {
             throw new IllegalArgumentException("Node cannot be null");
@@ -46,10 +81,27 @@ public final class Graph {
         }
     }
 
+    /**
+     * Validates two nodes for null-safety and affiliation to current graph
+     * @param from Node that need to be validated
+     * @param to Node that need to be validated
+     * @throws IllegalArgumentException if node is null, does not belong to current graph or 'from' and 'to' are the same object
+     * */
     private void validateNodes(Node from, Node to) {
         validateNode(from);
         validateNode(to);
         if (from.equals(to)) throw new IllegalArgumentException("Cannot connect one node to itself");
+    }
+
+    /**
+     * Check if two specified nodes are adjacent
+     * @param from Node that needs to be checked for adjacency
+     * @param to Node that needs to be checked for adjacency
+     * @return boolean that indicates whether specified nodes are adjacent
+     * */
+    public boolean checkAdjacency(Node from, Node to) {
+        validateNodes(from, to);
+        return nodes.get(from).containsKey(to);
     }
 
     public static final class Edge {
