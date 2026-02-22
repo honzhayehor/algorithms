200fb793d259f8b54846bd5d3c1c22dfb3c6d52a
Honzha Yehor <honzhayehor@gmail.com>
2026-02-22 21:37:58 +0200

    Added new private static class to make it easier to work with path following table. Further development of main Dijkstra method

--- src/main/java/graph/Dijkstra.java
+++ src/main/java/graph/Dijkstra.java
@@ -2,35 +2,51 @@ package graph;
 
 import org.jspecify.annotations.Nullable;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Stream;
 
 public final class Dijkstra {
     private Dijkstra() {}
 
+    private static final class DataHolder {
+        int bestDistance;
+        Graph.Node previous;
+
+        DataHolder(int bestDistance, Graph.Node previous) {
+            this.bestDistance = bestDistance;
+            this.previous = previous;
+        }
+    }
     /**
      * For given graph finds best sequence of nodes that is the shortest distance between two given nodes.
      * @param graph Graph that contains sequence of connected nodes
      * @param from Node from which the path has to be found
      * @param to Node to which path has to be found
-     * @return list of Nodes, e.g. path from start node to end node
+     * @return list of Nodes, e.g. path from start node to end node or empty list
      * @throws IllegalArgumentException if start or end node do not belong to given graph.
      * */
     public static List<Graph.Node> findPath(Graph graph, Graph.Node from, Graph.Node to) {
         // TODO: Implement main method here
-        if (!graph.containsNode(from) || !graph.containsNode(to)) throw new IllegalArgumentException("Node does not belong to given graph");
-        Map<Graph.Node, Integer> dist = new HashMap<>();
-        Map<Graph.Node, Graph.@Nullable Node> prev = new HashMap<>();
-
-        for (Graph.Node node : graph.asList()) {
-            dist.put(node, Integer.MAX_VALUE);
-            prev.put(node, null);
+        if (!graph.containsNode(from) || !graph.containsNode(to)) {
+            throw new IllegalArgumentException("Node does not belong to given graph");
         }
-        dist.put(from, 0);
+        record QNode(Graph.Node node, int dist) {}
 
+        Map<Graph.Node, DataHolder> table = new HashMap<>();
+        for (Graph.Node node : graph.asList()) {
+            table.put(node, new DataHolder(Integer.MAX_VALUE, null));
+        }
+        table.get(from).bestDistance = 0;
 
+        PriorityQueue<QNode> pq =
+                new PriorityQueue<>(Comparator.comparingInt(QNode::dist));
 
+        pq.add(new QNode(from, 0));
+
+        while (!pq.isEmpty()) {
+            QNode q = pq.poll();
+            Graph.Node u = q.node();
+        }
 
         return List.of();
     }
