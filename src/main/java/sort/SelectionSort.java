699c12737e916a63af4a24debd3a9e6b25bd21ed
Honzha Yehor <honzhayehor@gmail.com>
2026-02-27 11:08:45 +0200

    Changed selection sort to follow main selection sort algorithm (for some reason I made previous version overly complicated).

--- src/main/java/sort/SelectionSort.java
+++ src/main/java/sort/SelectionSort.java
@@ -2,47 +2,28 @@ package sort;
 
 public class SelectionSort {
     public static int[] sort(int[] arr) {
-        int[] sortedArr = new int[arr.length];
-        int[] currentArr = copyArr(arr);
         for (int i = 0; i < arr.length; i++) {
-            int smallestIdx = findSmallestIdx(currentArr);
-            int smallest = currentArr[smallestIdx];
-            sortedArr[i] = smallest;
-            currentArr = removeElement(currentArr, smallestIdx);
+            int smallestIdx = findSmallestIdx(arr, i, arr.length);
+
+            if (smallestIdx != i) {
+                int tmp = arr[i];
+                arr[i] = arr[smallestIdx];
+                arr[smallestIdx] = tmp;
+            }
         }
-        return sortedArr;
+        return arr;
     }
 
-    private static int findSmallestIdx(int[] arr) {
-        int smallest_idx = 0;
-        int smallest = arr[0];
+    private static int findSmallestIdx(int[] arr, int from, int to) {
+        int smallest_idx = from;
+        int smallest = arr[smallest_idx];
 
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = from + 1; i < to; i++) {
             if (arr[i] < smallest) {
                 smallest = arr[i];
                 smallest_idx = i;
             }
         }
         return smallest_idx;
-    }
-
-    private static int[] removeElement(int[] arr, int at) {
-        int[] newArr = new int[arr.length-1];
-        int counter = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (i != at) {
-                newArr[counter++] = arr[i];
-            }
-        }
-        return newArr;
-    }
-
-    private static int[] copyArr(int[] arr) {
-        int[] newArr = new int[arr.length];
-
-        for (int i = 0; i < arr.length; i++) {
-            newArr[i] = arr[i];
-        }
-        return newArr;
     }
 }
--- src/test/java/sort/SelectionSortTest.java
+++ src/test/java/sort/SelectionSortTest.java
@@ -7,9 +7,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class SelectionSortTest {
 
     @Test
-    void correctSortForIntArray() {
+    void correctSortForIntArray1() {
         int[] arrToSort = {12, 53, 22, 63, 45, 76, 98, 1, 2, 11, 2};
         int[] expected = {1, 2, 2, 11, 12, 22, 45, 53, 63, 76, 98};
+        int[] actual = SelectionSort.sort(arrToSort);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void correctSortForIntArray2() {
+        int[] arrToSort = {3, 2, 1, 4};
+        int[] expected = {1, 2, 3, 4};
         int[] actual = SelectionSort.sort(arrToSort);
 
         assertArrayEquals(expected, actual);
