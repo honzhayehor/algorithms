76c9925ef92469b23339cf7089715e60b99fb262
Honzha Yehor <honzhayehor@gmail.com>
2026-02-28 20:43:57 +0200

    Methods for all basic primitive arrays implemented.

--- src/main/java/sort/SelectionSort.java
+++ src/main/java/sort/SelectionSort.java
@@ -4,17 +4,18 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SelectionSort {
-    public static int[] sort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int smallestIdx = findSmallestIdx(arr, i, arr.length);
+    public static <T> List<T> sort(List<T> list, Comparator<? super T> comparator) {
+        for (int i = 0; i < list.size(); i++) {
+            int smallestIdx = findSmallestIdx(list, i, list.size(), comparator);
 
-            if (smallestIdx != i) {
-                int tmp = arr[i];
-                arr[i] = arr[smallestIdx];
-                arr[smallestIdx] = tmp;
+            if (smallestIdx != i) { // swap
+                T tmp = list.get(i);
+                T smallest = list.get(smallestIdx);
+                list.set(i, smallest);
+                list.set(smallestIdx, tmp);
             }
         }
-        return arr;
+        return list;
     }
     public static <T> T[] sort(T[] arr, Comparator<? super T> comparator) {
         for (int i = 0; i < arr.length; i++) {
@@ -29,6 +30,78 @@ public class SelectionSort {
         return arr;
     }
 
+    public static int[] sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int smallestIdx = findSmallestIdx(arr, i, arr.length);
+
+            if (smallestIdx != i) {
+                int tmp = arr[i];
+                arr[i] = arr[smallestIdx];
+                arr[smallestIdx] = tmp;
+            }
+        }
+        return arr;
+    }
+    public static byte[] sort(byte[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int smallestIdx = findSmallestIdx(arr, i, arr.length);
+
+            if (smallestIdx != i) {
+                byte tmp = arr[i];
+                arr[i] = arr[smallestIdx];
+                arr[smallestIdx] = tmp;
+            }
+        }
+        return arr;
+    }
+    public static long[] sort(long[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int smallestIdx = findSmallestIdx(arr, i, arr.length);
+
+            if (smallestIdx != i) {
+                long tmp = arr[i];
+                arr[i] = arr[smallestIdx];
+                arr[smallestIdx] = tmp;
+            }
+        }
+        return arr;
+    }
+    public static short[] sort(short[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int smallestIdx = findSmallestIdx(arr, i, arr.length);
+
+            if (smallestIdx != i) {
+                short tmp = arr[i];
+                arr[i] = arr[smallestIdx];
+                arr[smallestIdx] = tmp;
+            }
+        }
+        return arr;
+    }
+    public static float[] sort(float[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int smallestIdx = findSmallestIdx(arr, i, arr.length);
+
+            if (smallestIdx != i) {
+                float tmp = arr[i];
+                arr[i] = arr[smallestIdx];
+                arr[smallestIdx] = tmp;
+            }
+        }
+        return arr;
+    }
+    public static double[] sort(double[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int smallestIdx = findSmallestIdx(arr, i, arr.length);
+
+            if (smallestIdx != i) {
+                double tmp = arr[i];
+                arr[i] = arr[smallestIdx];
+                arr[smallestIdx] = tmp;
+            }
+        }
+        return arr;
+    }
     private static int findSmallestIdx(int[] arr, int from, int to) {
         int smallestIdx = from;
         int smallest = arr[smallestIdx];
@@ -115,19 +188,6 @@ public class SelectionSort {
         return smallestIdx;
     }
 
-    public static <T> List<T> sort(List<T> list, Comparator<? super T> comparator) {
-        for (int i = 0; i < list.size(); i++) {
-            int smallestIdx = findSmallestIdx(list, i, list.size(), comparator);
-
-            if (smallestIdx != i) { // swap
-                T tmp = list.get(i);
-                T smallest = list.get(smallestIdx);
-                list.set(i, smallest);
-                list.set(smallestIdx, tmp);
-            }
-        }
-        return list;
-    }
     private static <T> int findSmallestIdx(List<T> list, int from, int to, Comparator<? super T> comparator) {
         if (comparator == null) throw new IllegalArgumentException("Comparator cannot be null");
         int smallestIdx = from;
